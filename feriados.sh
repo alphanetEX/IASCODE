72de3e2c82ca5659ae651aa7dee74a2cfdf85c7b
AlphanetEX <24623630+alphanetEX@users.noreply.github.com>
2022-05-12 17:11:32 -0300

    added ternary if validations for print holiday days

--- feriados.sh
+++ feriados.sh
@@ -3,7 +3,6 @@
 source /opt/tp/scripts/general_print.sh
 unset query[3]
 
-
 function tercer_lunes {
 MES=$1
 # Calculo el tercer lunes del mes
@@ -13,10 +12,16 @@ done
 FTL=`date --date "$DIA 14 days" +'%d/%m/%Y'`
 }
 
+function esLaborable(){
+dateX=$1
+[[ -z  $dateX  ]] && dateX=$(date +"%Y/%m/%d")
 
-funcion esLaborable(){
-#!/bin/bash
-[ ! -f .env ] || export $(grep -v '^#' .env | xargs)
+if [[ $dateX =~ ^[0-9]*$ ]]; then 	
+    validateX=$(sed -r 's/.*([0-9]{4}).*([0-9]{2}).*([0-9]{2}).*/\1/\2/\3/' <<< $dateX)
+else
+	PrinterLog 3 "validacion de fecha" "la fecha $dateX al parecer no es una fecha"
+	echo "" > results.csv
+fi
 #stable
 mysql -u root -p${PASS_MYSQL_ROOT} feriados << EOF && mv /var/lib/mysql-files/results.csv $PWD/
 SELECT
@@ -26,7 +31,7 @@ feriados.titulo FROM
 tipo_feriado INNER JOIN
 feriados
 ON tipo_feriado.id_tferiado = feriados.id_tferiado
-WHERE feriados.fecha = "$1"
+WHERE feriados.fecha = "$validateX"
 INTO OUTFILE '/var/lib/mysql-files/results.csv'
 FIELDS TERMINATED BY ';'
 OPTIONALLY ENCLOSED BY '\"'
@@ -37,7 +42,9 @@ query[0]=$(sed -r 's/\"(.*)\"/\1/' <<< $(cut -d ';' -f1 results.csv))
 query[1]=$(sed -r 's/\"(.*)\"/\1/' <<< $(cut -d ';' -f2 results.csv))
 query[2]=$(sed -r 's/\"(.*)\"/\1/' <<< $(cut -d ';' -f3 results.csv))
 
-[[ -f ${query[0]} && -f ${query[1]} && -f ${query[2]} ]] && "" || echo "${query[1]}" "${query[2]}"
+#[[ -z ${query[0]} && -z ${query[1]} && -z ${query[2]} ]] || echo "${query[1]}" "${query[2]}"
+#comprobar esta validacion
+[[ -z ${query[0]} && -z ${query[1]} && -z ${query[2]} ]] || PrinterLog 1 "${query[1]}" "${query[2]}"
 
 }
 
